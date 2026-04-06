519eaa4003ce306e314f808158e7a093109a2c69
casey-brooks <casey.brooks@agyn.io>
2026-04-07 01:51:08 +0400

    fix(image-pull-secret): add organization id (#59)

--- examples/resources/agyn_image_pull_secret/resource.tf
+++ examples/resources/agyn_image_pull_secret/resource.tf
@@ -1,4 +1,9 @@
+resource "agyn_organization" "example" {
+  name = "example-org"
+}
+
 resource "agyn_image_pull_secret" "example" {
+  organization_id = agyn_organization.example.id
   description = "Example image pull secret."
   registry    = "registry.example.com"
   username    = "registry-user"
